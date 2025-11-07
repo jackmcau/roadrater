a63801c2359658cbc798a0f9e90e06f9b9246c7e
jackmcau <jackmcauliffe2006@gmail.com>
2025-11-07 12:35:05 -0700

    Some default prompts added, untested so far

---
+++ db/prompting.sql
@@ -0,0 +1,30 @@
+-- Get user from username
+
+SELECT *
+FROM users
+WHERE username = '${username}';
+
+-- Get all user's ratings from id
+
+SELECT ratings.*
+FROM users
+INNER JOIN ratings
+ON ratings.user_id = users.id
+WHERE users.username = '${username}';
+
+-- Get all ratings from a road
+
+SELECT ratings.*
+FROM roads
+INNER JOIN ratings
+ON ratings.road_id = roads.id
+WHERE roads.id = '${road_id}';
+
+-- Get top 5 rated roads
+
+SELECT roads.*, AVG(ratings.rating)
+FROM roads
+INNER JOIN ratings
+ON ratings.road_id = roads.id
+GROUP BY roads.id
+LIMIT 5;
