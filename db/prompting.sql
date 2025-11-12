4f4572d2287ab7f32e26d61599dcb1d689094703
jackmcau <jackmcauliffe2006@gmail.com>
2025-11-12 15:59:37 -0700

    added rating insert, delete, and edit

--- db/prompting.sql
+++ db/prompting.sql
@@ -29,3 +29,24 @@ ON ratings.road_id = roads.id
 GROUP BY roads.id
 ORDER BY AVG(ratings.rating) DESC
 LIMIT 5;
+
+-- New rating for given username, road_id
+
+INSERT INTO ratings (user_id, road_id, rating, created_at)
+VALUES (${userid}, ${roadid}, ${ratingnum}, GETDATE());
+
+-- Delete rating (Make sure user has correct credentials)
+
+DELETE FROM ratings
+WHERE user_id = '${user_id}' AND road_id = '${road_id}';
+
+-- OR
+
+DELETE FROM ratings
+WHERE id = '${rating_id}';
+
+-- Edit rating
+
+UPDATE ratings
+SET rating = ${rating}
+WHERE id = '${rating_id}'
